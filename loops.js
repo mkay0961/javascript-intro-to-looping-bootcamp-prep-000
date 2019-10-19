dbfdd21b179a035060d7a14451efa52a256116f5
Matthew Kay <mkay0961@gmail.com>
2019-10-19 18:50:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,13 +17,14 @@ function whileLoop(n) {
   return "done";
 }
 
-var i = 0
+var  i=0;
 
-function doWhileLoop(num) {
+function doWhileLoop(num){
+
   do {
-    console.log("I run once regardless")
-    i++
+    console.log ("I run once regardless.")
+    i++;
   }
-  while (i <= num)
-  return array;
+  while (i<=num);
+
 }
